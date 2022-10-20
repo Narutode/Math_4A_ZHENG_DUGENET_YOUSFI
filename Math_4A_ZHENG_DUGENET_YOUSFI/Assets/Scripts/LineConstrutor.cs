6a626661bdb2838b329aec1522d833be08bb8682
Soorry <77273729+Soorry@users.noreply.github.com>
2022-10-20 16:13:12 +0200

    Fix line rendering

--- Math_4A_ZHENG_DUGENET_YOUSFI/Assets/Scripts/LineConstrutor.cs
+++ Math_4A_ZHENG_DUGENET_YOUSFI/Assets/Scripts/LineConstrutor.cs
@@ -7,12 +7,12 @@ public class LineConstrutor : MonoBehaviour
     GameObject Sommet;
     public LineRenderer Line;
     private int tempcount = 0;
-
     public Camera cam;
     // Start is called before the first frame update
     void Start()
     {
-
+        Line.startWidth = .05f;
+        Line.endWidth = .05f;
     }
 
     // Update is called once per frame
@@ -20,14 +20,30 @@ public class LineConstrutor : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0)) // click gauche
         {
+            Vector3 point = new Vector3();
+            Event currentEvent = Event.current;
+
+            // Get the mouse position from Event.
+            // Note that the y position from Event is inverted.
+            //mousePos.x = currentEvent.mousePosition.x;
+            //mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
+            Vector2 mousePos = Input.mousePosition;
+
+            point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+
             Line.positionCount += 1;
-            Vector3 mousePos = Input.mousePosition;
             Sommet = new GameObject("Sommet");
-            Sommet.transform.position = cam.ScreenToWorldPoint(mousePos);;
-            Line.SetPosition(tempcount,Sommet.transform.position);
+            Sommet.transform.position = point;
+            Line.SetPosition(tempcount, Sommet.transform.position);
             tempcount++;
-            
         }
 
     }
+    /*
+    void OnGUI()
+    {
+       
+    }
+    
+    */
 }
