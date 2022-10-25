f6c9f8170120295fdecc219d44b0b81bb0afbcfe
KrymoXCIII <karym.yousfi@gmail.com>
2022-10-25 18:01:39 +0200

    import TMP UI & Camera setup

--- Math_4A_ZHENG_DUGENET_YOUSFI/Assets/Scripts/ContextMenu.cs
+++ Math_4A_ZHENG_DUGENET_YOUSFI/Assets/Scripts/ContextMenu.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ContextMenu : MonoBehaviour
 {
+    public GameObject menuPanel;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class ContextMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(1))
+        {
+            menuPanel.SetActive(true);
+        }
     }
 }
