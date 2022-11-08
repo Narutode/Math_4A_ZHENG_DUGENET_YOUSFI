c1f493f996acf5533952f448ac05b60e00a92714
KrymoXCIII <karym.yousfi@gmail.com>
2022-11-08 10:25:48 +0100

    Button attrib

--- Math_4A_ZHENG_DUGENET_YOUSFI/Assets/Scripts/ContextMenu.cs
+++ Math_4A_ZHENG_DUGENET_YOUSFI/Assets/Scripts/ContextMenu.cs
@@ -30,4 +30,29 @@ public class ContextMenu : MonoBehaviour
             
         }
     }
+
+    public void Couleurs()
+    {
+        Debug.Log("Couleurs");
+    }
+
+    public void Polygone()
+    {
+        Debug.Log("Polygone");
+    }
+
+    public void Tracé()
+    {
+        Debug.Log("Tracé");
+    }
+
+    public void Fenêtrage()
+    {
+        Debug.Log("Fenêtrage");
+    }
+
+    public void Remplissage()
+    {
+        Debug.Log("Remplissage");
+    }
 }
