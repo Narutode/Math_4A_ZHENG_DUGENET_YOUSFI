33094f53abd14af79fc0515104d1a28341b239ac
Soorry <77273729+Soorry@users.noreply.github.com>
2022-11-28 19:13:37 +0100

    Auto stash before merge of "main" and "origin/main"

--- Math_4A_ZHENG_DUGENET_YOUSFI/Assets/Scripts/Window.cs
+++ Math_4A_ZHENG_DUGENET_YOUSFI/Assets/Scripts/Window.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class Window
 {
@@ -81,5 +82,56 @@ public class Window
             return true;
         }
         return false;
+    }
+
+    public Boolean coupe(Vector2 P1, Vector2 P2, Vector2 P3, Vector2 P4)
+    {
+        float a = P2.x - P1.x;
+        float b = P3.x - P4.x;
+        float c = P2.y - P1.y;
+        float d = P3.y - P4.y;
+        float det = a * d - b * c;
+        return det != 0;
+    }
+    
+    public Vector2 intersect(Vector2 P1, Vector2 P2, Vector2 P3, Vector2 P4)
+    {
+        float a = P2.x - P1.x;
+        float b = P3.x - P4.x;
+        float c = P2.y - P1.y;
+        float d = P3.y - P4.y;
+
+        float e = P3.x - P1.x;
+        float f = P3.y - P1.y;
+        
+        float det = a * d - b * c;
+
+        float invA = a / det;
+        float invB = b / det;
+        float invC = c / det;
+        float invD = d / det;
+
+        float t = a * e + b * f;
+
+        return P1 + (P2 - P1) * t;
+    }
+
+    public Boolean visible(Vector2 P1, Vector2 P2, Vector2 n)
+    {
+        Vector2 dir = new Vector2(P1.x - P2.x, P1.x - P2.x);
+        double prodScalaire = Vector2.Dot(dir, n);
+        return prodScalaire >= 0;
+    }
+
+    public List<Vector2> SutherlandHodgman(List<Vector2> poly)
+    {
+        List<Vector2> newPoly;
+        for (int i = 0; i < Sommets.Count; i++)
+        {
+            //Vector2 C = Sommets[i];
+            int newPolyCount = 0;
+            
+        }
+        return newPoly;
     }
 }
