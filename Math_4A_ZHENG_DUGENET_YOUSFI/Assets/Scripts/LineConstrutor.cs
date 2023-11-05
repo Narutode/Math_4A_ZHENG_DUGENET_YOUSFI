f0d3703877aa6ea4c05c747219dc916bd6cd9678
Soorry <rdugenet@myges.fr>
2023-11-05 20:37:13 +0100

    wip triangulation

--- Math_4A_ZHENG_DUGENET_YOUSFI/Assets/Scripts/LineConstrutor.cs
+++ Math_4A_ZHENG_DUGENET_YOUSFI/Assets/Scripts/LineConstrutor.cs
@@ -8,23 +8,36 @@ using UnityEngine;
 using UnityEngine.Serialization;
 using Color = UnityEngine.Color;
 using UnityEngine.EventSystems;
+using static FonctionMath;
+
+public struct Segments
+{
+    public Vector2 Point1;
+    public Vector2 Point2;
+}
+
+public struct Triangles
+{
+    public Segments Seg1;
+    public Segments Seg2;
+    public Segments Seg3;
+}
 
 public class LineConstrutor : MonoBehaviour
 {
     public Camera cam;
-    GameObject Sommet;
+    GameObject _sommet;
     [FormerlySerializedAs("Line")] public LineRenderer curLine;
 
     public GameObject pointGO;
-    public List<GameObject> points;
+    public List<GameObject> listGameObjects;
+    public List<Vector2> listPoints;
+    public List<Segments> ListSegments = new List<Segments>();
+    public List<Triangles> ListTriangles = new List<Triangles>();
+
     public List<LineRenderer> lines;
-    private float nearClipPlaneWorldPoint = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
+    private float _nearClipPlaneWorldPoint = 0;
+    
     // Update is called once per frame
     void Update()
     {
@@ -35,58 +48,110 @@ public class LineConstrutor : MonoBehaviour
             Vector2 mousePos = Input.mousePosition;
 
             point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane + 1f));
-            if (nearClipPlaneWorldPoint == 0)
-                nearClipPlaneWorldPoint = point.z;
+            if (_nearClipPlaneWorldPoint == 0)
+                _nearClipPlaneWorldPoint = point.z;
             pointGO.transform.position = point;
             GameObject newP = Instantiate(pointGO);
-            if (points.Count > 0)
+            /*
+            if (listGameObjects.Count > 0)
             {
                 GameObject newGO = new GameObject();
                 newGO.name = "sides";
                 LineRenderer newLine = newGO.AddComponent<LineRenderer>();
                 newLine.positionCount = 2;
-                newLine.SetPosition(0,points.Last().transform.position);
+                newLine.SetPosition(0,listGameObjects.Last().transform.position);
                 newLine.SetPosition(1,newP.transform.position);
                 lines.Add(newLine);
             }
-            points.Add(newP);
-            if (points.Count == 3)
+            */
+            listGameObjects.Add(newP);
+            listPoints.Add(newP.transform.position);
+            
+            if (listGameObjects.Count == 10)
             {
-                GameObject newGO = new GameObject();
-                newGO.name = "sides";
-                LineRenderer newLineEnd = newGO.AddComponent<LineRenderer>();
-                newLineEnd.positionCount = 2;
-                newLineEnd.SetPosition(0,points.Last().transform.position);
-                newLineEnd.SetPosition(1,points.First().transform.position);
-                lines.Add(newLineEnd);
-                Vector3[] normals = fonctionMath.getNormals(points[0].transform.position, points[1].transform.position, points[2].transform.position);
-                int index = 0;
-                foreach (var n in normals)
+                /*
+                //List<Vector2> jarvis = GetJarvis(listPoints);
+                List<Vector2> jarvis = GetGrahamScan(listPoints);
+                int size = jarvis.Count;
+                for (int i = 0; i < size; i++)
                 {
-                    GameObject newNormals = new GameObject();
-                    newNormals.name = "Normals";
-                    LineRenderer newLine = newNormals.AddComponent<LineRenderer>();
+                    GameObject newGO = new GameObject();
+                    newGO.name = "sides";
+                    LineRenderer newLine = newGO.AddComponent<LineRenderer>();
                     newLine.positionCount = 2;
-                    Vector3 newPos = (points[(index + 1) % 3].transform.position + points[index].transform.position)/2f;
-                    newPos.z = points[index].transform.position.z;
-                    newLine.SetPosition(0,newPos);
-                    newLine.SetPosition(1,newPos+n);
+                    newLine.SetPosition(0,new Vector3(jarvis[i].x,jarvis[i].y, _nearClipPlaneWorldPoint));
+                    newLine.SetPosition(1,new Vector3(jarvis[(i+1)%size].x,jarvis[(i+1)%size].y, _nearClipPlaneWorldPoint));
                     lines.Add(newLine);
-                    index++;
-                }
+                }*/
+                // Sort the points (assuming ascending order)
+                listPoints = listPoints.OrderBy(p => p.x).ThenBy(p => p.y).ToList();
 
-                foreach (var line in lines)
+                // Create the initial triangle
+                Vector2 p0 = listPoints.First();
+                Vector2 p1 = listPoints.Skip(1).First();
+                Vector2 p2 = listPoints.Skip(2).First();
+
+                ListSegments.Add(new Segments { Point1 = p0, Point2 = p1 });
+                ListSegments.Add(new Segments { Point1 = p1, Point2 = p2 });
+                ListSegments.Add(new Segments { Point1 = p2, Point2 = p0 });
+
+                ListTriangles.Add(new Triangles { Seg1 = ListSegments[0], Seg2 = ListSegments[1], Seg3 = ListSegments[2] });
+
+                foreach (var curP in listPoints.Skip(3))
                 {
-                    line.startWidth = .05f;
-                    line.endWidth = .05f;
-                }
+                    List<Segments> listSegmentsToDelete = new List<Segments>();
 
-                Vector3 centerCircle = fonctionMath.getCenterCircle(points[0].transform.position, points[1].transform.position, points[2].transform.position);
+                    foreach (var freeP in listPoints)
+                    {
+                        if (ListSegments.Any(t => (t.Point1 == freeP && t.Point2 == curP) || (t.Point1 == curP && t.Point2 == freeP)))
+                        {
+                            Segments curS = ListSegments.FirstOrDefault(t => (t.Point1 == freeP && t.Point2 == curP) || (t.Point1 == curP && t.Point2 == freeP));
+                            if (!listSegmentsToDelete.Contains(curS))
+                                listSegmentsToDelete.Add(curS);
+                        }
+                    }
+
+                    foreach (var segment in listSegmentsToDelete)
+                    {
+                        ListSegments.Remove(segment);
+                    }
+
+                    foreach (var segment in listSegmentsToDelete)
+                    {
+                        ListSegments.Add(new Segments { Point1 = curP, Point2 = segment.Point1 });
+                        ListSegments.Add(new Segments { Point1 = segment.Point2, Point2 = curP });
+
+                        ListTriangles.Add(new Triangles { Seg1 = segment, Seg2 = ListSegments.Last(), Seg3 = ListSegments[ListSegments.Count - 2] });
+                    }
+                }
+                
+                int size = ListTriangles.Count;
+                Debug.Log(size);
+                for (int i = 0; i < size; i++)
+                {
+                    GameObject newGO = new GameObject();
+                    newGO.name = "sides";
+                    LineRenderer newLine = newGO.AddComponent<LineRenderer>();
+                    newLine.positionCount = 6;
+                    newLine.SetPosition(0,new Vector3(ListTriangles[i].Seg1.Point1.x,ListTriangles[i].Seg1.Point1.y, _nearClipPlaneWorldPoint));
+                    newLine.SetPosition(1,new Vector3(ListTriangles[i].Seg1.Point2.x,ListTriangles[i].Seg1.Point2.y, _nearClipPlaneWorldPoint));
+                    newLine.SetPosition(2,new Vector3(ListTriangles[i].Seg2.Point1.x,ListTriangles[i].Seg2.Point1.y, _nearClipPlaneWorldPoint));
+                    newLine.SetPosition(3,new Vector3(ListTriangles[i].Seg2.Point2.x,ListTriangles[i].Seg2.Point2.y, _nearClipPlaneWorldPoint));
+                    newLine.SetPosition(4,new Vector3(ListTriangles[i].Seg3.Point1.x,ListTriangles[i].Seg3.Point1.y, _nearClipPlaneWorldPoint));
+                    newLine.SetPosition(5,new Vector3(ListTriangles[i].Seg3.Point2.x,ListTriangles[i].Seg3.Point2.y, _nearClipPlaneWorldPoint));
+                    lines.Add(newLine);
+                }
+            }
+
+            /*
+                Vector3 centerCircle = fonctionMath.getCenterCircle(listPoints[0].transform.position, listPoints[1].transform.position, listPoints[2].transform.position);
                 centerCircle.z = nearClipPlaneWorldPoint;
                 pointGO.transform.position = centerCircle;
                 GameObject center = Instantiate(pointGO);
                 center.name = "center";
-            }
+            */
         }
     }
+    
+    
 }
--- Math_4A_ZHENG_DUGENET_YOUSFI/Assets/Scripts/fonctionMath.cs
+++ Math_4A_ZHENG_DUGENET_YOUSFI/Assets/Scripts/fonctionMath.cs
@@ -1,31 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using UnityEngine;
 
-public static class fonctionMath
+public static class FonctionMath
 {
-    public static bool testVisibilité(Vector2 normal, Vector2 line)
+    public static bool TestVisibilité(Vector2 normal, Vector2 line)
     {
         return Vector3.Dot(normal, line) > 0;
     }
     
-    public static float angle(Vector2 normal, Vector2 line)
+    public static float Angle(Vector2 normal, Vector2 line)
     {
         return Mathf.Acos(Vector3.Dot(normal, line)/(normal.magnitude*line.magnitude));
     }
 
-    public static bool testInteriorité(Vector2 normal1, Vector2 normal2, Vector2 normal3, Vector2 line)
+    public static bool isConvexe(Vector2 normal, Vector2 line)
     {
-        if (testVisibilité(normal1, line))
+        return (Mathf.Acos(Vector3.Dot(normal, line)/(normal.magnitude*line.magnitude)) > Mathf.PI);
+    }
+    
+    public static bool TestInteriorité(Vector2 normal1, Vector2 normal2, Vector2 normal3, Vector2 line)
+    {
+        if (TestVisibilité(normal1, line))
             return false;
-        if (testVisibilité(normal2, line))
+        if (TestVisibilité(normal2, line))
             return false;
-        if (testVisibilité(normal3, line))
+        if (TestVisibilité(normal3, line))
             return false;
         return true;
     }
 
-    public static Vector3[] getNormals(Vector3 point1, Vector3 point2, Vector3 point3)
+    public static Vector3[] GETNormals(Vector3 point1, Vector3 point2, Vector3 point3)
     {
         Vector3[] normals = new Vector3[3];
         normals[0] = new Vector3(point2.y - point1.y, -(point2.x - point1.x), 0).normalized;
@@ -37,25 +44,106 @@ public static class fonctionMath
         return normals;
     }
 
-    public static Vector3 getCenterCircle(Vector2 point1, Vector2 point2, Vector2 point3)
+    public static Vector3 GETCenterCircle(Vector2 point1, Vector2 point2, Vector2 point3)
     {
-        float[,] M = new float[2, 2];
-        float[] XY = new float[2];
-        M[0, 0] = point2.x - point1.x;
-        M[1, 0] = point3.x - point2.x;
-        M[0, 1] = point2.y - point1.y;
-        M[1, 1] = point3.y - point2.y;
-        float detM = M[0, 0] * M[1, 1] - M[1, 0] * M[0, 1];
+        float[][] m = new float[2][];
+        for (int index = 0; index < 2; index++)
+        {
+            m[index] = new float[2];
+        }
+
+        float[] xy = new float[2];
+        m[0][0] = point2.x - point1.x;
+        m[1][0] = point3.x - point1.x;
+        m[0][1] = point2.y - point1.y;
+        m[1][1] = point3.y - point1.y;
+        float detM = m[0][0] * m[1][1] - m[1][0] * m[0][1];
         if(Mathf.Abs(detM) < 0.0001f)
             return new Vector3();
-        float[,] Minv = new float[2, 2];
-        Minv[0, 0] = M[1, 1] / detM;
-        Minv[1, 0] = -M[1, 0] / detM;
-        Minv[0, 1] = -M[0, 1] / detM;
-        Minv[1, 1] = M[0, 0] / detM;
-        XY[0] = point2.x * point2.x + point2.y * point2.y - point1.x * point1.x - point1.y * point1.y;
-        XY[1] = point3.x * point3.x + point3.y * point3.y - point2.x * point2.x - point2.y * point2.y;
-        Vector3 res = new Vector3(XY[0]*Minv[0, 0] + XY[1]*Minv[0, 1], XY[0]*Minv[1, 0] + XY[1]*Minv[1, 1]);
+        float[][] minv = new float[2][];
+        for (int index = 0; index < 2; index++)
+        {
+            minv[index] = new float[2];
+        }
+        minv[0][0] = m[1][1] / detM;
+        minv[1][0] = -m[1][0] / detM;
+        minv[0][1] = -m[0][1] / detM;
+        minv[1][1] = m[0][0] / detM;
+        xy[0] = (point2.x * point2.x + point2.y * point2.y - point1.x * point1.x - point1.y * point1.y)/2;
+        xy[1] = (point3.x * point3.x + point3.y * point3.y - point1.x * point1.x - point1.y * point1.y)/2;
+        Vector3 res = new Vector3(xy[0]*minv[0][0] + xy[1]*minv[0][1], xy[0]*minv[1][0] + xy[1]*minv[1][1]);
         return res;
+    }
+
+    public static List<Vector2> GetJarvis(List<Vector2> pointList)
+    {
+        List<Vector2> jarvis = new List<Vector2>();
+        
+        //On trouve le point le plus à gauche et on le retire de la liste
+        Vector2 startPoint = pointList.First();
+        foreach (var point in pointList)
+        {
+            if (point.x < startPoint.x)
+                startPoint = point;
+        }
+        jarvis.Add(startPoint);
+        
+        Vector2 curPoint = startPoint;
+
+        do
+        {
+            Vector2 curNormal = new Vector2(curPoint.y, -curPoint.x);
+            Vector2 nextPoint = pointList.First();
+            float smallestAngle = Mathf.PI;
+            foreach (var point in pointList)
+            {
+                if (point != curPoint)
+                {
+                    //On compare les angles et on stock le plus petit
+                    float curAngle = Angle(curNormal, point - curPoint);
+                    if (curAngle < smallestAngle)
+                    {
+                        smallestAngle = curAngle;
+                        nextPoint = point;
+                    }
+                }
+            }
+            curPoint = nextPoint;
+            pointList.Remove(curPoint);
+            jarvis.Add(curPoint);
+        } while (curPoint != startPoint);
+
+        return jarvis;
+    }
+
+    public static List<Vector2> GetGrahamScan(List<Vector2> pointList)
+    {
+        List<Vector2> grahamScan = new List<Vector2>();
+
+        // Trouver le point le plus bas (et le plus à gauche) comme point de départ
+        Vector2 startPoint = pointList.OrderBy(p => p.y).ThenBy(p => p.x).First();
+
+        // Trier les points par angle par rapport au point de départ
+        List<Vector2> sortedPoints = pointList.OrderBy(p => Mathf.Atan2(p.y - startPoint.y, p.x - startPoint.x)).ToList();
+
+        grahamScan.Add(startPoint);
+        grahamScan.Add(sortedPoints[0]);
+
+        for (int i = 1; i < sortedPoints.Count; i++)
+        {
+            while (grahamScan.Count > 1 && !IsConvex(grahamScan[^2], grahamScan.Last(), sortedPoints[i]))
+            {
+                grahamScan.RemoveAt(grahamScan.Count - 1);
+            }
+            grahamScan.Add(sortedPoints[i]);
+        }
+
+        return grahamScan;
+    }
+    
+    private static bool IsConvex(Vector2 p0, Vector2 p1, Vector2 p2)
+    {
+        float crossProduct = (p1.x - p0.x) * (p2.y - p0.y) - (p2.x - p0.x) * (p1.y - p0.y);
+        return crossProduct >= 0;
     }
 }
